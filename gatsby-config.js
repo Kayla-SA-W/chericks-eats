66d98a9c2a581e71399d0040e5416d0cf07d94f5
kayla williams <shalisha.angelique@gmail.com>
2026-01-23 14:46:52 -0500

    Update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,14 +11,7 @@ module.exports = {
           path: `${__dirname}/src/images`
         },
     },
-    {
-      resolve: 'gatsby-plugin-tslint',
-      options: {
-        test: /\.ts$|\.tsx$/,
-        exclude: /(node_modules|cache|public)/
-      }
-    },
-      `gatsby-plugin-image`,
+    `gatsby-plugin-image`,
       `gatsby-transformer-sharp`, 
       `gatsby-plugin-sharp`,
       `gatsby-plugin-styled-components`
