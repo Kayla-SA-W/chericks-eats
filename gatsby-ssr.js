0619dc37bc0a40ebbc05782b4833f09df505f017
kayla williams <shalisha.angelique@gmail.com>
2026-05-15 16:14:47 -0400

    Fix build

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,3 +4,8 @@ import { MealPrepCartProvider } from './src/context/meal-prep-cart';
 export const wrapRootElement = ({ element }) => (
   <MealPrepCartProvider>{element}</MealPrepCartProvider>
 );
+
+export const onRenderBody = ({ setBodyAttributes, setHtmlAttributes }) => {
+  setHtmlAttributes({ style: { backgroundColor: '#1a0e07' } });
+  setBodyAttributes({ style: { backgroundColor: '#1a0e07', margin: 0 } });
+};
